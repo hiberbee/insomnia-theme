e0591bb7b5101f985a65327619b9c801dab777d1
Vlad Volkov <vlad.volkov@bndigital.co>
2021-03-21 19:52:10 +0200

    Automated flow

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 // For help writing plugins, visit the documentation to get started:
 //   https://support.insomnia.rest/article/173-plugins
-module.exports.themes = [
-    require('./themes/dark'),
-]
+module.exports.themes = [require("./themes/dark")];
--- src/themes/dark.js
+++ src/themes/dark.js
@@ -3,7 +3,7 @@ module.exports = {
   displayName: "Hiberbee",
   theme: {
     foreground: {
-      default: "#F9F6EF"
+      default: "#F9F6EF",
     },
     background: {
       default: "#282828",
@@ -12,7 +12,7 @@ module.exports = {
       warning: "#FF8C00",
       danger: "#ED005C",
       surprise: "#7D7AFF",
-      info: "#409CFF"
+      info: "#409CFF",
     },
     styles: {
       paneHeader: {
@@ -23,28 +23,28 @@ module.exports = {
           warning: "#EE9B70",
           danger: "#EE7762",
           surprise: "#AB9DF2",
-          info: "#70D7FF"
-        }
+          info: "#70D7FF",
+        },
       },
       foreground: {
-        default: '#cccccc',
+        default: "#cccccc",
       },
       background: {
-        default: '#323232',
+        default: "#323232",
       },
       dialog: {
         background: {
-          default: "#323232"
-        }
+          default: "#323232",
+        },
       },
       transparentOverlay: {
         background: {
-          default: "rgba(30, 29, 28, 0.9)"
+          default: "rgba(30, 29, 28, 0.9)",
         },
         foreground: {
-          default: "rgba(249,246,239,0.9)"
-        }
-      }
-    }
-  }
+          default: "rgba(249,246,239,0.9)",
+        },
+      },
+    },
+  },
 };
